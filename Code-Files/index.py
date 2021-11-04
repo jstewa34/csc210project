65d8eb535ec4aded52434a867b663868c423abdc
jstewa34 <73316380+jstewa34@users.noreply.github.com>
2021-11-04 13:47:43 -0400

    um

--- Code-Files/index.py
+++ Code-Files/index.py
@@ -23,12 +23,6 @@ mail = Mail(app)
 def landing():
 	return render_template('Landing.html')
 
-
-@app.route('/gamepage/<string:username>') #right now just using username as unique url. We can change this
-def gamepage(username=None):
-	return render_template('Game-Page.html', uname=username)
-
-
 class loginForm(FlaskForm):
 	username = StringField('Username:', validators=[DataRequired()])
 	password = StringField('Password:', validators=[DataRequired()])
@@ -43,12 +37,11 @@ def login():
 	if form.validate_on_submit():
 		username = form.username.data
 		password = form.password.data
-		print(password)
 		# We will need to check here if the user already exists in the data base 
 			# if they do exist continue with current code	
 			# Otherwise 
 				#return redirect(url_for('createaccount'))
-		return redirect(url_for('gamepage', username=username))
+		return render_template('Game-Page.html', uname=username)
 	return render_template('Login.html', form=form, username=username, password=password)
 
 
@@ -83,7 +76,7 @@ def createaccount():
 				#return redirect(url_for('login'))
 			# Otherwise continues with current code
 		# This is where password hashing will happen
-		return redirect(url_for('gamepage', username=username))
+		return render_template('Game-Page.html', uname=username)
 	return render_template('Create-Account.html', form=form, fname=fname , lname=lname , username=username, password=password, email=email)
 
 
